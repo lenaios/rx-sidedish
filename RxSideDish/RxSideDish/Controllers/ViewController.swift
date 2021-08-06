688fb4f4811cf638060481a7b6ad06f670d72cd4
lena <sue822330@gmail.com>
2021-08-07 05:20:10 +0900

    Update to use tableView.rx.itemSelected

--- RxSideDish/RxSideDish/Controllers/ViewController.swift
+++ RxSideDish/RxSideDish/Controllers/ViewController.swift
@@ -36,17 +36,33 @@ class ViewController: UIViewController {
   }
   
   private func bind() {
-    viewModel.subject
-      .subscribe { event in
-        DispatchQueue.main.sync {
-          if let indexSet = event.element {
-            self.tableView.reloadSections(
-              indexSet,
-              with: .automatic)
-          }
-        }
-      }
+    tableView.rx.itemSelected
+      .asDriver()
+      .drive { self.showDetail(for: $0) }
       .disposed(by: disposeBag)
+
+    viewModel.subject
+      .subscribe(onNext: { data in
+        DispatchQueue.main.sync {
+          self.tableView.reloadSections(data, with: .automatic)
+        }
+      })
+      .disposed(by: disposeBag)
+  }
+}
+
+private extension ViewController {
+  func showDetail(for indexPath: IndexPath) {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    guard
+      let detailViewController = storyboard.instantiateViewController(
+        withIdentifier: SideDishDetailViewController.identifier) as? SideDishDetailViewController else {
+      return
+    }
+    let sideDish = viewModel.sideDish(at: indexPath)
+    let service = RepositoryService<SideDishResponse>(sessionManager: SessionManager.shared)
+    detailViewController.viewModel = SideDishDetailViewModel(repositoryService: service, model: sideDish)
+    navigationController?.pushViewController(detailViewController, animated: true)
   }
 }
 
@@ -64,20 +80,5 @@ extension ViewController: UITableViewDelegate {
     let title = viewModel.header(at: section)
     header.configure(title: title)
     return header
-  }
-  
-  func tableView(
-    _ tableView: UITableView,
-    didSelectRowAt indexPath: IndexPath) {
-    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    guard
-      let detailViewController = storyboard.instantiateViewController(
-        withIdentifier: SideDishDetailViewController.identifier) as? SideDishDetailViewController else {
-      return
-    }
-    let sideDish = viewModel.sideDish(at: indexPath)
-    let service = RepositoryService<SideDishResponse>(sessionManager: SessionManager.shared)
-    detailViewController.viewModel = SideDishDetailViewModel(repositoryService: service, model: sideDish)
-    navigationController?.pushViewController(detailViewController, animated: true)
   }
 }
